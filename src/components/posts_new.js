253e63430870f12c33d1d70dc4a466dc6dabbe84
temirka13 <temirka13@mail.ru>
2018-04-24 20:30:57 +0300

    142

---
+++ src/components/posts_new.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+
+  renderField(field) {
+
+    const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`;
+
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        <div className="text-help">
+          {field.meta.touched ? field.meta.error : ''}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    this.props.createPost(values);
+  }
+
+  render() {
+
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title for Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if(!values.categories) {
+    errors.categories = "Enter some categories.";
+  }
+  if(!values.content) {
+    errors.content = "Enter some content please.";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(
+  connect(null, { createPost })(PostsNew)
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as fromReducer } from 'redux-form';
+import PostsReducer from './reducer_posts';
 
 
 const rootReducer = combineReducers({
-
+  posts: PostsReducer,
+  form: fromReducer
 });
 
 export default rootReducer;

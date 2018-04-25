c04f2b9ee0b0c8fdd4dfedb9cd61b5c06818f0d1
temirka13 <temirka13@mail.ru>
2018-04-25 21:27:48 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
-import PostIndex from './components/post_index';
+import PostIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostIndex} />
         </Switch>
       </div>
